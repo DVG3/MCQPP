7ed1bdc7490ddea725e2ef14f3f32da7de0d8da1
Trinh Minh <lapminh02022006@gmail.com>
2025-05-16 12:27:40 +0700

    Add GP

--- paths.js
+++ paths.js
@@ -1,9 +1,14 @@
 const tests = [
+"GP/Cơ đầu mặt cổ.json",
+"GP/Khớp học tổng quát.json",
+"GP/Miệng răng lưỡi TNB.json",
+"GP/Mạch máu.json",
+"GP/Tạng học.json",
+"GPB/TT-RLTN TH-Vận dụng cao.json",
 "Mô/Mô cơ.json",
 "Mô/Mô liên kết - Sụn - Xương.json",
 "Mô/Mô máu.json",
 "Mô/Mô thần kinh.json",
 "Phôi/Phôi tuần 3-4 - đề 1.json",
 "Phôi/Phôi tuần 3-4 - đề 2.json",
-"GPB/TT-RLTN TH-Vận dụng cao.json",
 ];
